eaf748341496bc861b8b3b93dee200cd8a4fa0fd
adik08pl <adik08.email@gmail.com>
2023-06-14 16:09:41 +0200

    commit 2

--- src/main/java/com/example/kantor/HelloController.java
+++ src/main/java/com/example/kantor/HelloController.java
@@ -12,10 +12,82 @@ import java.net.URL;
 public class HelloController {
     public Label lb1, lb2, lb3, lb4, lb5, lb6, lb7, lb8, lb9, lb10;
 
-    public void initialize() throws IOException
-    {
+    public String jsonString;
+
+    public void initialize() throws IOException {
+
+        laczenie("http://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json");
+        //JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray osobyArray = jsonObject.getJSONArray("rates");
+        JSONObject osobaObject = osobyArray.getJSONObject();
+
+        String kupUSD = osobaObject.getString("imie");
+        kupUSD = Double.parseDouble(kupUSD) + 0,22;
+        String sprzedajUSD = osobaObject.getString("imie");
+        sprzedajUSD = Double.parseDouble(sprzedajUSD);
+
+        lb1.setText(kupUSD);
+        lb2.setText(sprzedajUSD);
+
+
+        laczenie("http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json");
+        //JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray osobyArray = jsonObject.getJSONArray("rates");
+        JSONObject osobaObject = osobyArray.getJSONObject();
+
+        String kupEUR = osobaObject.getString("imie");
+        kupEUR = Double.parseDouble(kupEUR) + 0,22;
+        String sprzedajEUR = osobaObject.getString("imie");
+        sprzedajEUR = Double.parseDouble(sprzedajEUR);
+
+        lb3.setText(kupEUR);
+        lb4.setText(sprzedajEUR);
+
+
+        laczenie("http://api.nbp.pl/api/exchangerates/rates/a/chf/?format=json");
+        //JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray osobyArray = jsonObject.getJSONArray("rates");
+        JSONObject osobaObject = osobyArray.getJSONObject();
+
+        String kupCHF = osobaObject.getString("imie");
+        kupCHF = Double.parseDouble(kupCHF) + 0,22;
+        String sprzedajCHF = osobaObject.getString("imie");
+        sprzedajCHF = Double.parseDouble(sprzedajCHF);
+
+        lb5.setText(kupCHF);
+        lb6.setText(sprzedajCHF);
+
+
+        laczenie("http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json");
+        //JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray osobyArray = jsonObject.getJSONArray("rates");
+        JSONObject osobaObject = osobyArray.getJSONObject();
+
+        String kupGBP = osobaObject.getString("imie");
+        kupGBP = Double.parseDouble(kupGBP) + 0,22;
+        String sprzedajGBP = osobaObject.getString("imie");
+        sprzedajGBP = Double.parseDouble(sprzedajGBP);
+
+        lb7.setText(kupGBP);
+        lb8.setText(sprzedajGBP);
+
+
+        laczenie("http://api.nbp.pl/api/exchangerates/rates/a/cny/?format=json");
+        //JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray osobyArray = jsonObject.getJSONArray("rates");
+        JSONObject osobaObject = osobyArray.getJSONObject();
+
+        String kupCNY = osobaObject.getString("imie");
+        kupCNY = Double.parseDouble(kupCNY) + 0,22;
+        String sprzedajCNY = osobaObject.getString("imie");
+        sprzedajCNY = Double.parseDouble(kupCNY);
+
+        lb9.setText(kupCNY);
+        lb10.setText(sprzedajCNY);
+    }
+
+    public void laczenie(String apiUrl) throws IOException {
         try {
-            String apiUrl = "http://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json";
             URL url = new URL(apiUrl);
 
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -35,12 +107,10 @@ public class HelloController {
 
                 String jsonResponse = response.toString();
                 System.out.println("Odpowiedź JSON: " + jsonResponse);
+                jsonString = jsonResponse;
+            }
 
-
-                // Możesz tutaj parsować i przetwarzać JSON
-
-
-            } else {
+            else {
                 System.out.println("Błąd podczas pobierania danych. Kod odpowiedzi: " + responseCode);
             }
             connection.disconnect();
@@ -49,4 +119,6 @@ public class HelloController {
         }
     }
 }
+
+
 
